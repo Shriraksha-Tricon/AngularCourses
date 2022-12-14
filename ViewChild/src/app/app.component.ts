b99c4617cb0c859d9687b1a1483df97bdd39fa29
Shriraksha-Bhat <67705206+Shriraksha-Bhat@users.noreply.github.com>
2022-12-14 16:02:55 +0530

    Viewchild

--- ViewChild/src/app/app.component.ts
+++ ViewChild/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ViewChild';
+
+  @ViewChild('dobInput') dateOfBirth!: ElementRef;
+  @ViewChild('ageInput') age!: ElementRef;
+  @ViewChild(DemoComponent, { static: true })
+  demoComp!: DemoComponent;
+
+  calculateAge(){
+      let birthDate = new Date(this.dateOfBirth.nativeElement.value);
+      let currentDate = new Date();
+      let diffInMilliseconds = Math.abs(currentDate.getTime() - birthDate.getTime()); 
+      let diffInYears = diffInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+      this.age.nativeElement.value = Math.floor(diffInYears);
+  }
+
 }

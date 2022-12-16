388d2906bf87657b2f51b96ed18aa3c763a63b24
Shriraksha-Bhat <67705206+Shriraksha-Bhat@users.noreply.github.com>
2022-12-16 17:18:29 +0530

    Custom Directive

--- CustomDirective/src/app/CustomDirectives/setbackground.directive.ts
+++ CustomDirective/src/app/CustomDirectives/setbackground.directive.ts
@@ -1,11 +1,19 @@
-import { Directive, ElementRef } from "@angular/core";
+import { Directive, ElementRef, OnInit } from "@angular/core";
 
 @Directive({
     selector: '[setBackground]'
 })
 
-export class setBackgroundDirective{
+export class setBackgroundDirective implements OnInit{
+
+    private element: ElementRef;
+
     constructor(element: ElementRef){
-        element.nativeElement.style.backgroundColor = '#C8E6C9';
+        //element.nativeElement.style.backgroundColor = '#C8E6C9';
+        this.element = element;
+    }
+
+    ngOnInit(){
+        this.element.nativeElement.style.backgroundColor = '#C8E6C9';
     }
 }

6ec8043e7195d2af9cb8bed6d59a28b807229bcf
Shriraksha-Bhat <67705206+Shriraksha-Bhat@users.noreply.github.com>
2022-12-19 14:47:33 +0530

    ToDoList Application

--- ToDoList/src/app/app.component.ts
+++ ToDoList/src/app/app.component.ts
@@ -9,17 +9,6 @@ export class AppComponent implements OnInit {
   title = 'TO DO LIST';
   list: any [] = [];
 
-
-/* @HostListener('window:load')onLoad() {
-    const value = localStorage.getItem('TodoList')
-    if(localStorage){
-      let value = localStorage.getItem('TodoList')
-      if(value!=null){
-        console.log(value)
-      }
-    }
- } */
-
 ngOnInit(){
   this.list = JSON.parse(localStorage.getItem('TodoList') || '{}');
 }

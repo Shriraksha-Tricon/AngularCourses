9c67465f9fa03902d0b0dab2ce1b4e6d422783ff
Shriraksha-Bhat <67705206+Shriraksha-Bhat@users.noreply.github.com>
2022-12-20 11:58:11 +0530

    Conditional Directive

--- DirectiveExample/src/app/app.component.ts
+++ DirectiveExample/src/app/app.component.ts
@@ -9,16 +9,15 @@ export class AppComponent {
   title = 'DirectiveExample';
 
   videos= [
-    {title: 'My Video 1', share: 415, likes: 257, dislikes: 12, tumbline: 'assets/images/img-1.jpg'},
-    {title: 'My Video 2', share: 215, likes: 325, dislikes: 12, tumbline: 'assets/images/img-2.jpg'},
-    {title: 'My Video 3', share: 513, likes: 105, dislikes: 12, tumbline: 'assets/images/img-3.jpg'}
+    {title: 'My video 1', share: 415, likes: 257, dislikes: 12, thumbnail: 'assets/images/image1.jpg'},
+    {title: 'My video 2', share: 215, likes: 325, dislikes: 12, thumbnail: 'assets/images/image2.jpg'},
+    {title: 'My video 3', share: 513, likes: 105, dislikes: 12, thumbnail: 'assets/images/image3.jpg'}
   ]
 
-  mostLikedVideo = this.getMostLikedVideo();
+  mostLikedVideo = this.getmostlikedVideo();
 
-  getMostLikedVideo(){
+  getmostlikedVideo(){
     let videoCopy = [...this.videos];
-    return videoCopy.sort((curt, next) => next.likes - curt.likes)[0];
+    return videoCopy.sort((curr, next) => next.likes - curr.likes)[0];
   }
-
 }

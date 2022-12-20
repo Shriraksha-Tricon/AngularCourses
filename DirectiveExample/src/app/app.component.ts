8f177c9bba1406f40ac58e40a7345479fa08f557
Shriraksha-Bhat <67705206+Shriraksha-Bhat@users.noreply.github.com>
2022-12-20 11:00:31 +0530

    Conditional Directive

---
+++ DirectiveExample/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DirectiveExample';
+
+  videos= [
+    {title: 'My Video 1', share: 415, likes: 257, dislikes: 12, tumbline: 'assests/images/img-1.jpg'},
+    {title: 'My Video 2', share: 215, likes: 325, dislikes: 12, tumbline: 'assests/images/img-2.jpg'},
+    {title: 'My Video 3', share: 513, likes: 105, dislikes: 12, tumbline: 'assests/images/img-3.jpg'}
+  ]
+}

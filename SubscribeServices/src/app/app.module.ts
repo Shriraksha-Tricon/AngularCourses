47e0c7eb70a32656cffd43330c1261baf00dc724
Shriraksha-Bhat <67705206+Shriraksha-Bhat@users.noreply.github.com>
2022-12-21 12:32:46 +0530

    Services

--- SubscribeServices/src/app/app.module.ts
+++ SubscribeServices/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { JavaScriptComponent } from './java-script/java-script.component';
+import { AngularComponent } from './angular/angular.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JavaScriptComponent,
+    AngularComponent
   ],
   imports: [
     BrowserModule
